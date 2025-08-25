0132b646c56030f8082eaddbc03c425a1b9a74c2
Batubo Victory <victorybatubo76@gmail.com>
2025-08-25 09:26:13 +0100

    New 6kyu

--- src/js/script.js
+++ src/js/script.js
@@ -245,7 +245,7 @@ Happy Coding!
 function squareDigits(num){
   let string = String(num)
   let newNum = [];
-  
+
   for(char of string){
     newNum.push(char * char)
   }
@@ -253,4 +253,16 @@ function squareDigits(num){
   return Number(newNum.join(''))
 }
 
-console.log(squareDigits(34971))
+
+
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
