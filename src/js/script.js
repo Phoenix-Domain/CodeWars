1212349aee0cd41865204178f53941bbcb17cc72
Batubo Victory <victorybatubo76@gmail.com>
2025-08-27 14:38:07 +0100

    Daily log: 27/08/2025

--- src/js/script.js
+++ src/js/script.js
@@ -306,3 +306,31 @@ function accum(s) {
 
   return duplicate.join('-')  //join it with a '-' and return
 }
+
+
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+// Examples
+// [1, 1, 2] ==> 2
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+
+
+function stray(numbers) {
+  const frequency = {};
+  //loop through the array
+  for(num of numbers){
+    frequency[num] = (frequency[num] || 0) + 1;
+  }
+
+  for(key in frequency){
+    if(frequency[key] === 1){
+      return key
+    }
+  }
+}
+console.log(stray([1, 1, 2])) //2
+console.log(stray([17, 17, 3, 17, 17, 17, 17])) //3
