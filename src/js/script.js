9eb4b72256ff122a2a907c8ca95d3d17ffb556ae
Batubo Victory <victorybatubo76@gmail.com>
2025-09-22 11:30:11 +0100

    Daily log: 22-09-2025

--- src/js/script.js
+++ src/js/script.js
@@ -461,3 +461,16 @@ function count(string) {
   return charCount
 }
 
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+  const split = str.split(' ');
+
+  const words = split.map(word => word.split('').reverse().join(''))
+
+  return words.join(' ')
+}
