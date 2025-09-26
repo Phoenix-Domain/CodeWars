1f816ea3aea0c12eb0348cf00dcc7508e7bcbff8
Batubo Victory <victorybatubo76@gmail.com>
2025-09-26 18:03:27 +0100

    Daily log: 26-09-2025

--- src/js/script.js
+++ src/js/script.js
@@ -493,5 +493,22 @@ function between(a, b) {
   return arr
 }
 
+// Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+// For example, when the input is green, output should be yellow.
+
 
 
